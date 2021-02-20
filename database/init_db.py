9f26c574b3abded4e110813eb94f723365e3148c
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2021-02-20 23:27:13 +0900

    append

--- database/init_db.py
+++ database/init_db.py
@@ -7,13 +7,14 @@ from .schema import Base, User
 def init_db():
     clear_db()
     create_table()
+    write_element()
 
 def clear_db():
     print('clear DB')
     sql         = 'SELECT relname AS table_name FROM pg_stat_user_tables;'
     result      = engine.execute(sql).fetchall()
     table_list  = [dict(row.items()).get('table_name') for row in result ]
-    table_list.remove('user')
+    if 'user' in table_list: table_list.remove('user')
     for table in table_list:
         sql     = f'DROP TABLE IF EXISTS {table};'
         engine.execute(sql)
@@ -28,6 +29,6 @@ def write_element():
     session         = SessionClass()
     user_list       = [ User(   first_name="first_b{}".format(i),
                                 last_name="last_b{}".format(i),
-                                age=20+i) for i in range(10) ]
+                                age=20+i) for i in range(20) ]
     for user in user_list: session.add(user)
     session.commit()
