dbc26236381292f3a926a2e1a076c204b158a871
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2021-02-20 21:51:37 +0900

    append

--- database/engine.py
+++ database/engine.py
@@ -2,14 +2,8 @@
 from sqlalchemy import create_engine
 from .util import get_uri
 
-engine = None
-
-def init_engine():
-    global engine
-    engine = create_engine(
-        get_uri(),
-        encoding = "utf-8",
-        echo=True
-    )
-
-init_engine()
+engine = create_engine(
+    get_uri(),
+    encoding = "utf-8",
+    echo=True
+)
