16ff5fbe72dc1d7ba7dd9f7b3250613434e3d19c
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2021-03-07 13:51:22 +0900

    append

--- spa/src/App.js
+++ spa/src/App.js
@@ -2,28 +2,42 @@ import './App.css';
 import React from 'react'
 import ListView from './components/list'
 import AddButton from './components/pushbutton'
+import Sidebar from "react-sidebar";
 
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    // this.setState({ sidebarOpen: open });
+    this.setState({sidebarOpen: !this.state.sidebarOpen})
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <AddButton/>
-          <ListView title='mytitle'/>
-        </header>
+        <div className="App-main">
+          <div className="App-sidebar-container">
+            <div className={this.state.sidebarOpen?"App-sidebar sidebar-open":"App-sidebar"}/>
+          </div>
+          <div className="App-header">
+            <button onClick={() => this.onSetSidebarOpen(true)}>
+              Open sidebar
+            </button>
+          </div>
+          <div className="App-content">
+            <AddButton />
+            <ListView title='mytitle' />
+          </div>
+        </div>
       </div>
     )
   }
